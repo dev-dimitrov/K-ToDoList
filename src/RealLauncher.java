202f28c90599ece25743d2f0c30f5e4e541f44a5
su-kaizen <badboynodo313@gmail.com>
2025-08-08 23:48:59 +0300

    feature(add-items): added the list methods, all of this is provisional bc I will figure out how to make it with other type of data. Created too the resources package

--- src/RealLauncher.java
+++ src/RealLauncher.java
@@ -1,4 +1,7 @@
-package PACKAGE_NAME;
-
 public class RealLauncher {
+    public static void main(String[] args) {
+
+        // You have to launch here, if you try to launch it form the actual main, it crashes and idk why
+        LauncherToDo.main(args);
+    }
 }
