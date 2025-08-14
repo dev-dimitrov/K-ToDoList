96f7b6da6af2fdde69c7d522148fa2adfd769170
su-kaizen <badboynodo313@gmail.com>
2025-08-15 00:30:59 +0300

    feature(add-task-showing): now the task information is showing, not totally correcty btw

--- src/ControllerToDo.java
+++ src/ControllerToDo.java
@@ -34,11 +34,15 @@ public class ControllerToDo  implements Initializable {
     // private List<String> sample = List.of("Wash the dishes", "Take out the trash", "Water the plants", "Vacuum the floor", "Walk the dog", "Clean the bathroom", "Fold the clothes", "Sweep the floor", "Feed the cat", "Organize the desk");
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        System.out.println("Al empezar: "+taskList.getStyle());
+        toggleItems(false);
         taskList.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Task>() {
             @Override
             public void changed(ObservableValue<? extends Task> observableValue, Task previous, Task selected) {
                 // manage here the selected task (This is not implying to check the checkbox)
+                titleLabel.setText(selected.title);
+                descTextArea.setText(selected.description);
+                creationLabel.setText("Created on: "+selected.getCreationDate());
+                toggleItems(true);
             }
         });
     }
@@ -53,5 +57,12 @@ public class ControllerToDo  implements Initializable {
     public void addTask(String task){
         Task t = new Task(task,"No desc", LocalDateTime.now());
         taskList.getItems().add(t);
+    }
+
+    public void toggleItems(boolean t){
+        titleLabel.setVisible(t);
+        creationLabel.setVisible(t);
+        descTextArea.setVisible(t);
+        saveButton.setVisible(t);
     }
 }
